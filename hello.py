ac1c6d797ad58fcf8b587d0517ba9cd5c3a6b2de
Philip Puccia <93366234+pucciap@users.noreply.github.com>
2022-10-26 08:16:20 -0400

    Found a better snake

--- hello.py
+++ hello.py
@@ -1,21 +1,35 @@
 #this is my first Python program on GitHub
 name = input("What is your name? ")
-response = "Hi, " + name + " My name is Python! hssss"
+response = "Hi, " + name + " My name is Monty!"
 print("")
 print("")
 print(response)
-print("")
-print("")
-print("                 ~~~~~")
-print("                /  @ @|")
-print("               / ____~~~**")
-print("              / /   ")
-print("              | |")
-print("              | |")
-print("   ~~         |")
-print("  // )        | |")
-print(" //   )       | |")
-print("//            / /")
-print("||           / /")
-print("||~~~~~~~~~~/ /")
-print("~~~~~~~~~~~~~~")
+print("""
+
+
+
+""")
+
+print(r""" 
+
+
+       ---_ ......._-_--.
+      (|\ /      / /| \  \
+      /  /     .'  -=-'   `.
+     /  /    .'             )
+   _/  /   .'        _.)   /
+  / o   o        _.-' /  .'
+  \          _.-'    / .'*|
+   \______.-'//    .'.' \*|
+            //   .'.' _ |*|
+     `   \|//  .'.'_ _ _|*|
+      .  .// .'.' | _ _ \*|
+      \`-|\_/ /    \ _ _ \*\
+       `/'\__/      \ _ _ \*\
+      /^|            \ _ _ \*
+     '  `             \ _ _ \      ASH (+VK)
+                       \_
+
+
+
+               """)
